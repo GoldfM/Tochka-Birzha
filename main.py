aa76e46419c7232ddf13662a0a8411e12cf91ceb
Михаил Гольдфарб <goldfarbm@mail.ru>
2025-05-30 01:00:55 +0500

    models v1

---
+++ main.py
@@ -0,0 +1,10 @@
+import uvicorn
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+#TODO: маршруты
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8080)
---
+++ models.py
@@ -0,0 +1,79 @@
+from pydantic import BaseModel
+from typing import List
+from uuid import UUID
+from enum import Enum
+
+class Role(Enum):
+    USER = "user"
+    ADMIN = "admin"
+
+class OrderState(str, Enum):
+    CREATED = "CREATED"
+    FILLED = "FILLED"
+    PARTIALLY_FILLED = "PARTIALLY_FILLED"
+    CANCELED = "CANCELED"
+
+class TradeDirection(str, Enum):
+    BUY = "buy"
+    SELL = "sell"
+
+class UserNameNew(BaseModel):
+    name: str
+
+class User(BaseModel):
+    id: UUID
+    name: str
+    role: Role = Role.USER
+    api_key: str
+
+class Level(BaseModel):
+    price: int
+    qty: int
+
+class OrderBook(BaseModel):
+    bid_levels: List[Level]
+    ask_levels: List[Level]
+
+class Transaction(BaseModel):
+    ticker: str
+    amount: int
+    price: int
+    timestamp: str
+
+class BalanceUp(BaseModel):
+    id: UUID
+    ticker: str
+    amount: int
+
+class BalanceDown(BaseModel):
+    id: UUID
+    ticker: str
+    amount: int
+
+
+
+class LimitOrderRequest(BaseModel):
+    side: TradeDirection
+    symbol: str
+    quantity: int
+    limit_price: int
+
+class MarketOrderRequest(BaseModel):
+    side: TradeDirection
+    symbol: str
+    quantity: int
+
+class LimitOrder(BaseModel):
+    order_id: UUID
+    status: OrderState
+    trader_id: UUID
+    created_at: str
+    details: LimitOrderRequest
+    executed_qty: int = 0
+
+class MarketOrder(BaseModel):
+    order_id: UUID
+    status: OrderState
+    trader_id: UUID
+    created_at: str
+    details: MarketOrderRequest
