fb56ea02a5551773bea2aa56b7ca67105f1ab54b
Timofey <pre_first@mail.ru>
2025-06-09 21:29:37 +0500

    deploy 2

--- apply_migrations.sh
+++ apply_migrations.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Ждём готовности PostgreSQL
-until pg_isready -h rc1a-2q0g5tca2ag46g51.mdb.yandexcloud.net -p 6432 -U user -d tochka; do
+until pg_isready -h rc1a-i25i7m8rrl055efp.mdb.yandexcloud.net -p 6432 -U user -d tochka; do
   echo "Waiting for PostgreSQL to be ready..."
   sleep 1
 done
@@ -9,8 +9,8 @@ done
 echo "PostgreSQL is ready - applying migrations..."
 
 # Применяем миграции
-export PGPASSWORD="1234"
-psql -h rc1a-2q0g5tca2ag46g51.mdb.yandexcloud.net -p 6432 -U user -d tochka -f "/app/migrations/init.sql"
+export PGPASSWORD="12341234"
+psql -h rc1a-i25i7m8rrl055efp.mdb.yandexcloud.net -p 6432 -U user -d tochka -f "app/migrations/init.sql"
 
 # Запускаем приложение
 echo "Starting application..."
